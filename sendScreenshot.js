2bc9abad4eff652855435a8a0073c8b66b7f9e89
Giovanni Rella <giovanni.rella@speedcast.com>
2020-09-11 10:40:17 -0400

    feature: ENHANCE

--- sendScreenshot.js
+++ sendScreenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 const { web } = require('./slackClient')
 
 
-const sendScreenshot = async (event, query) => {
+const sendScreenshot = async (event, query, firstImageOnly) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -14,6 +14,11 @@ const sendScreenshot = async (event, query) => {
     height: 1024
   })
   await page.goto(`https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch`)
+  if (firstImageOnly) {
+    await page.click('div.islrc > div > a');
+    const firstImageUrl = await page.evaluate(() => decodeURIComponent(document.getElementsByClassName('islrc')[0].firstChild.firstChild.href.match(/imgurl=(.*?)&/).pop()));
+    await page.goto(firstImageUrl);
+  }
   let data = await page.screenshot()
   await browser.close()
   
