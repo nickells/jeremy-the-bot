4d2915e67791f82f0bd3236a5afd3bcfed050ba0
Nick Ellsworth <nellsworth@spotify.com>
2020-10-19 11:48:05 -0400

    Jermy sends the img instead of a screenshot of the img

--- server/sendScreenshot.js
+++ server/sendScreenshot.js
@@ -1,7 +1,26 @@
-
 const puppeteer = require('puppeteer')
 const { web } = require('./slackClient')
 
+const https = require('https');
+const http = require('http')
+
+const getBufferFromRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    const protocol = url.startsWith('https') ? https : http;
+    
+    protocol.get(url, resp => {
+      const dataArray = []
+      resp.on('data', (data) => { 
+        dataArray.push(data)
+      });
+      resp.on('end', () => {
+        resolve(Buffer.concat(dataArray))
+      });
+    }).on('error', (e) => {
+      reject(e.message)
+    });
+  })
+}
 
 const sendScreenshot = async (event, query, firstImageOnly) => {
   const browser = await puppeteer.launch({
@@ -13,27 +32,20 @@ const sendScreenshot = async (event, query, firstImageOnly) => {
     width: 1280,
     height: 1024
   })
+
   await page.goto(`https://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch`)
+
+  // either a page screenshot or an img
+  let data
+
   if (firstImageOnly) {
     await page.click('div.islrc > div > a');
     const firstImageUrl = await page.evaluate(() => decodeURIComponent(document.getElementsByClassName('islrc')[0].firstChild.firstChild.href.match(/imgurl=(.*?)&/).pop()));
-    await page.goto(firstImageUrl);
-    await page.evaluate(() => {
-      const img = document.getElementsByTagName('img')[0]
-      const { width, height } = img
-      const { innerWidth, innerHeight } = window
-      if (width > height) {
-        if (width < innerWidth) {
-          img.style.transform = `scale(${innerWidth / width})`
-        }
-      }
-      else if (height < innerHeight) {
-        img.style.transform = `scale(${innerHeight / height})`
-      }
-    })
+    data = await getBufferFromRequest(firstImageUrl)
+  } else {
+    data = await page.screenshot()
   }
-  let data = await page.screenshot()
-  console.log(data)
+
   await browser.close()
   
   await web.files.upload({
